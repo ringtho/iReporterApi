6de739e4b2c7b35f63dd8c381f9f1e14e3608e7c
Smith Ringtho <sringtho@gmail.com>
2018-12-27 07:20:33 +0300

    add class for redflag in models.py

---
+++ api/models.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from random import randint
+
+class RedFlag:
+
+    """
+    class for creating endpoints for a redflag record
+    """
+    def __init__(self,**kwargs):
+        self.id = kwargs['id']
+        self.createdOn = datetime.today()
+        self.createdBy = createdBy['createdBy']
+        self.types = types['types']
+        self.location = kwargs['location']
+        self.status = kwargs['status']
+        self.images = kwargs['images']
+        self.videos = kwargs['videos']
+        self.comment = kwargs['comment']
+
+    def json_format(self):
+        format = {
+        "id": self.id,
+        "createdOn": self.createdOn,
+        "createdBy":self.createdBy,
+        "types": self.types,
+        "location": self.location,
+        "status": self.status,
+        "images": self.images,
+        "videos": self.videos,
+        "comment": self.comment
+        }
+        return format
