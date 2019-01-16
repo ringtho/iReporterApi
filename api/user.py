cd0a365adc38ff328315ee29d3c111c476eaba93
Smith Ringtho <sringtho@gmail.com>
2019-01-16 21:34:30 +0300

    add user.py file to contain user model

---
+++ api/user.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+count = 1
+
+class User:
+
+    def __init__(self, **kwargs):
+        global count
+        admin = 0
+        self.id = count
+        self.firstname = kwargs['firstname']
+        self.lastname = kwargs['lastname']
+        self.othernames = kwargs['othernames']
+        self.email = kwargs['email']
+        self.phoneNumber = kwargs['phoneNumber']
+        self.username = kwargs['username']
+        self.registered = datetime.today()
+        self.isAdmin = admin
+        self.password = generate_password_hash(kwargs['password'])
+        count+=0
+
+    def json_format(self):
+        format = {
+            "id": self.id,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "othernames": self.othernames,
+            "email": self.email,
+            "phoneNumber": self.phoneNumber,
+            "username": self.username,
+            "registered": self.registered,
+            "isAdmin": self.isAdmin
+        }
+        return format
