5077022d29efcef0d66a1c39ebcea134992a9494
Smith Ringtho <sringtho@gmail.com>
2019-01-17 22:26:49 +0300

    add test for registering user

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -118,6 +118,28 @@ class TestRedFlags(unittest.TestCase):
         self.assertEqual(data["data"][0]["message"], "red-flag record has been deleted")
         self.assertEqual(data["status"], 200)
 
+    def test_create_user(self):
+        user = {
+	
+            "firstname": "smith",
+            "lastname": "Ringtho",
+            "othernames": "J",
+            "email": "sringtho@gmail.com",
+            "phoneNumber": "0778339655",
+            "username": "sringtho1",
+            "password": "sr654321"
+
+            }
+        response = self.test_client.post("/api/v1/auth/register",
+        headers=dict(Authorization='Bearer '+ GetTokenTests.get_user_post(self)),json=user)
+        self.assertEqual(response.status_code, 201)
+        data = json.loads(response.data)
+        print(data)
+        self.assertEqual(data["status"], 201)
+        self.assertEqual(data["data"][0]["message"], "User successfully created")
+        self.assertEqual(data["data"][0]["user"]["username"], "sringtho1")
+
+
     def test_createdBy_string(self):
         incident = { "createdBy": "user", "types":"redflag", "location":"Arua", 
         "status": "accepted", "images": ["image.jpg","image2"], "videos": ["videos.mp4","smith.mkv"], 
