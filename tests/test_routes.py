9fae04b9448c6b6556eb3e42454a10bce0f48b84
Smith Ringtho <sringtho@gmail.com>
2019-01-17 22:39:57 +0300

    add test for when a required field is not provided

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -130,14 +130,34 @@ class TestRedFlags(unittest.TestCase):
             "password": "sr654321"
 
             }
-        response = self.test_client.post("/api/v1/auth/register",
-        headers=dict(Authorization='Bearer '+ GetTokenTests.get_user_post(self)),json=user)
+        response = self.test_client.post("/api/v1/auth/register",json=user)
         self.assertEqual(response.status_code, 201)
         data = json.loads(response.data)
         print(data)
         self.assertEqual(data["status"], 201)
         self.assertEqual(data["data"][0]["message"], "User successfully created")
         self.assertEqual(data["data"][0]["user"]["username"], "sringtho1")
+
+    def test_add_user_missing_username(self):
+        user = {
+	
+            "firstname": "smith",
+            "lastname": "Ringtho",
+            "othernames": "J",
+            "email": "sringtho@gmail.com",
+            "phoneNumber": "0778339655",
+            "password": "sr654321"
+
+        }
+        response = self.test_client.post("/api/v1/auth/register",json=user)
+        self.assertEqual(response.status_code, 200)
+        data = json.loads(response.data)
+        print(data)
+        self.assertEqual(data["status"], 400)
+        self.assertEqual(data["Error"], "username not specified")
+
+    
+
 
 
     def test_createdBy_string(self):
