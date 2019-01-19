01c667a042accd63ef72254d9a71b16195d7154b
Smith Ringtho <sringtho@gmail.com>
2019-01-19 07:55:52 +0300

    add the input data in setup function

--- tests/test_user_routes.py
+++ tests/test_user_routes.py
@@ -9,38 +9,41 @@ class TestUserRoutes(unittest.TestCase):
     def setUp(self):
         """initialise test client"""
         self.test_client = app.test_client()
-
-    def test_create_user(self):
-        user = {
+        self.user = {
 	
             "firstname": "smith",
             "lastname": "Ringtho",
             "othernames": "J",
             "email": "sringtho@gmail.com",
             "phoneNumber": "0778339655",
-            "username": "sringtho1",
+            "username": "sringtho",
             "password": "sr654321"
 
-            }
-        response = self.test_client.post("/api/v1/auth/register",json=user)
+        }
+        self.user_name={
+            "firstname": "smith",
+            "lastname": "Ringtho",
+            "othernames": "J",
+            "email": "sringtho@gmail.com",
+            "phoneNumber": "0778339655",
+            "password": "sr654321"
+        }
+        self.user_login = {
+            "username": "sringtho",
+            "password": "sr654321"
+        }
+
+    def test_create_user(self):
+        response = self.test_client.post("/api/v1/auth/register",json=self.user)
         self.assertEqual(response.status_code, 201)
         data = json.loads(response.data)
         # print(data)
         self.assertEqual(data["status"], 201)
         self.assertEqual(data["data"][0]["message"], "User successfully created")
-        self.assertEqual(data["data"][0]["user"]["username"], "sringtho1")
-    def test_add_user_missing_username(self):
-        user = {
-	
-            "firstname": "smith",
-            "lastname": "Ringtho",
-            "othernames": "J",
-            "email": "sringtho@gmail.com",
-            "phoneNumber": "0778339655",
-            "password": "sr654321"
+        self.assertEqual(data["data"][0]["user"]["username"], "sringtho")
 
-        }
-        response = self.test_client.post("/api/v1/auth/register",json=user)
+    def test_add_user_missing_username(self):
+        response = self.test_client.post("/api/v1/auth/register",json=self.user_name)
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         # print(data)
@@ -48,51 +51,26 @@ class TestUserRoutes(unittest.TestCase):
         self.assertEqual(data["Error"], "username not specified")
 
     def test_login(self):
-        user = {
-	
-            "firstname": "smith",
-            "lastname": "Ringtho",
-            "othernames": "J",
-            "email": "sringtho@gmail.com",
-            "phoneNumber": "0778339655",
-            "username": "sringtho",
-            "password": "sr654321"
-
-        }
-        response = self.test_client.post("/api/v1/auth/register",json=user)
+      
+        response = self.test_client.post("/api/v1/auth/register",json=self.user)
         self.assertEqual(response.status_code, 201)
-        user_login = {
-            "username": "sringtho",
-            "password": "sr654321"
-            }
-        response=self.test_client.post("/api/v1/auth/login" ,json=user_login)
+        response=self.test_client.post("/api/v1/auth/login" ,json=self.user_login)
         data = json.loads(response.data)
         # print(data)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(data["status"], 201)
         self.assertEqual(data["data"][0]["message"], "Logged in successfully")
 
-        # def test_incorrect_login_details(self):
-    #     user = {
-	
-    #         "firstname": "smith",
-    #         "lastname": "Ringtho",
-    #         "othernames": "J",
-    #         "email": "sringtho@gmail.com",
-    #         "phoneNumber": "0778339655",
-    #         "username": "sringtho",
-    #         "password": "sr654321"
-
-    #         }
-    #     response = self.test_client.post("/api/v1/auth/register",json=user)
-    #     self.assertEqual(response.status_code, 201)
-    #     user_login = {
-    #         "username": "smith",
-    #         "password": "sr654321"
-    #         }
-    #     response=self.test_client.post("/api/v1/auth/login" ,json=user_login)
-    #     data = json.loads(response.data)
-    #     print(data)
-    #     self.assertEqual(response.status_code, 201)
-    #     self.assertEqual(data["status"], 201)
-    #     self.assertEqual(data["data"][0]["message"], "Logged in successfully")
+    def test_incorrect_login_details(self):
+        response = self.test_client.post("/api/v1/auth/register",json=self.user)
+        self.assertEqual(response.status_code, 201)
+        user_login = {
+            "username": "smith",
+            "password": "sr654321"
+            }
+        response=self.test_client.post("/api/v1/auth/login" ,json=user_login)
+        data = json.loads(response.data)
+        print(data)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data["status"], 400)
+        self.assertEqual(data["Error"], "Invalid Username or Password")
