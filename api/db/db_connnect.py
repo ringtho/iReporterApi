19c6a494b3182cdff61aaa491c9d4b35ce31c406
Smith Ringtho <sringtho@gmail.com>
2019-01-22 10:38:17 +0300

    add connection to ireporter database

--- api/db/db_connnect.py
+++ api/db/db_connnect.py
@@ -6,8 +6,12 @@ class Database:
 
     def __init__(self):
         try:
-            self.conn = psycopg2.connect(environ.get("DATABASE_URL"))
+            # self.conn = psycopg2.connect(environ.get("DATABASE_URL"))
+            self.conn = psycopg2.connect("dbname='ireporter' user='postgres' host='localhost' password='.Adgjmp1' ")
             self.cursor = self.conn.cursor(cursor_factory=RealDictCursor)
             self.conn.autocommit = True
+            print("connected")
         except (Exception, psycopg2.Error) as e:
             print(e)
+
+Database()
