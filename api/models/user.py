3d5ba9ca5d1697639fb74ca8526c7c7e9fb929cc
Smith Ringtho <sringtho@gmail.com>
2019-01-25 08:02:22 +0300

    add function to get a specific user

--- api/models/user.py
+++ api/models/user.py
@@ -31,10 +31,17 @@ class User:
 
     def get_all_users(self):
         cursor = Database().get_cursor()
-        query = f"SELECT * FROM users"
+        query = f"SELECT firstname,email, id, lastname,username,phoneNumber, isAdmin FROM users"
         cursor.execute(query)
         users = cursor.fetchall()
         return users
+
+    def get_particular_user(self, user_id):
+        cursor = Database().get_cursor()
+        query = f"SELECT firstname,email, id, lastname,username,phoneNumber, isAdmin FROM users WHERE id={user_id}"
+        cursor.execute(query)
+        user = cursor.fetchone()
+        return user
 
     def delete_particular_user(self, user_id):
         cursor = Database().get_cursor()
@@ -42,6 +49,14 @@ class User:
         cursor.execute(query)
         rows = cursor.rowcount
         return rows
+
+    def get_id_signup(self):
+        cursor = Database().get_cursor()
+        query = "SELECT id FROM users ORDER BY id DESC"
+        cursor.execute(query)
+        user_id = cursor.fetchone()["id"]
+        return user_id
+
 
 
         
